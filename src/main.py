bf6777d40658eba2d36f3cf29f9328b39160a500
zassou-robo <zassou.sub@gmail.com>
2026-01-19 01:17:13 +0900

    各ファイルを呼び出し実行する処理を追加

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-def main():
-    print("Robocon Operator Assist - init")
-    # TODO: pad init
-    # TODO: mode select
-    # TODO: sim + control loop
+from input.dualshock import DualShock
+from control.mecanum import Mecanum
+from sim.viz import Viz
+import time
 
+def main():
+    pad = DualShock()
+    mech = Mecanum()
+    viz = Viz()
+
+    mode = "mecanum"
+
+    x = 0; y = 0; theta = 0
+    dt = 0.02
+
+    while True:
+        lx, ly, rx = pad.read()
+
+        wheels = mech.inverse(lx, ly, rx)
+        theta += rx * dt
+        x += lx * dt
+        y += ly * dt
+
+        viz.draw(x, y, theta, wheels, mode)
+        time.sleep(dt)
 
 if __name__ == "__main__":
     main()
