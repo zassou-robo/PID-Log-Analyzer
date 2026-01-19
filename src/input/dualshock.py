139a4037914c0868d25ac15d7066c7f3a1873e96
zassou-robo <zassou.sub@gmail.com>
2026-01-19 10:51:44 +0900

    旋回時の向き、スピードを変更

--- src/input/dualshock.py
+++ src/input/dualshock.py
@@ -4,6 +4,7 @@ class DualShock:
     def __init__(self):
         pygame.init()
         pygame.joystick.init()
+        print(pygame.joystick.get_count())
         self.js = pygame.joystick.Joystick(0)
         self.js.init()
 
@@ -11,5 +12,5 @@ class DualShock:
         pygame.event.pump()
         lx = self.js.get_axis(0)
         ly = -self.js.get_axis(1)  # forward+
-        rx = self.js.get_axis(2)
+        rx = -5 * self.js.get_axis(2)
         return lx, ly, rx
